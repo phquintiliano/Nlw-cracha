de7cf4d7093d818d28557ef7c467eabe8b583fb4
Pedro Quintiliano <86140675+Pedrinhk@users.noreply.github.com>
2021-10-24 14:02:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const linksSocialMedia = {
+  github: 'Pedrinhk',
+  facebook: 'pedrohquintiliano',
+  instagram: 'quintiliano_ph',
+  twitter: 'Pedro_LaikUS'
+}
+
+function chanheSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+chanheSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
